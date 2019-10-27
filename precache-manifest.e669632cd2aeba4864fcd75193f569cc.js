900ec79bdfa642efcf4ccb061656f69ab512fc5c
luetkemj <luetkemj@gmail.com>
2019-10-26 19:34:45 -0500

    Updates

---
+++ precache-manifest.e669632cd2aeba4864fcd75193f569cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8df5ee671fe853b1cc0bd3c944b3415",
+    "url": "/snail/index.html"
+  },
+  {
+    "revision": "50b50fc4e87aae5aabf3",
+    "url": "/snail/static/css/main.ca0023f0.chunk.css"
+  },
+  {
+    "revision": "89adef5ca5c7a2e411ac",
+    "url": "/snail/static/js/2.a262e2db.chunk.js"
+  },
+  {
+    "revision": "50b50fc4e87aae5aabf3",
+    "url": "/snail/static/js/main.2d9e58f5.chunk.js"
+  },
+  {
+    "revision": "09b36b301188ff00dddd",
+    "url": "/snail/static/js/runtime-main.bdf5c6ca.js"
+  }
+]);
